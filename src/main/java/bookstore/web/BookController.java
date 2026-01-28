35d57663d9e5afaae7ac29b066035a8a14a159d1
Jar1tsu <175611427+jar1tsu@users.noreply.github.com>
2026-01-28 20:33:55 +0200

    Bookstore alustettu

---
+++ src/main/java/bookstore/web/BookController.java
@@ -0,0 +1,16 @@
+package bookstore.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class BookController {
+
+    @GetMapping("/index")
+    public String showMessage() {
+        return "Welcome to bookstore!";
+    }
+}
