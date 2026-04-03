c1b09cefc7ff1c5c9143cc9234e1c9e6f634ef10
Jar1tsu <175611427+jar1tsu@users.noreply.github.com>
2026-04-03 21:30:58 +0300

    Ulkoinen tietokanta tehty

--- src/main/java/bookstore/BookstoreApplication.java
+++ src/main/java/bookstore/BookstoreApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.beans.factory.annotation.Value;
 
 import bookstore.domain.Book;
 import bookstore.domain.BookRepository;
@@ -20,6 +21,12 @@ import bookstore.domain.UserRepository;
 @SpringBootApplication
 public class BookstoreApplication {
 
+    @Value("${app.user.password}")
+    private String userPassword;
+
+    @Value("${app.admin.password}")
+    private String adminPassword;
+
     private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(BookstoreApplication.class, args);
@@ -27,30 +34,33 @@ public class BookstoreApplication {
 
     @Bean
     public CommandLineRunner bookDemo(BookRepository bookRepository, CategoryRepository categoryRepository, UserRepository userRepository) {
-    return (args) -> {
+        return (args) -> {
 
-        if (categoryRepository.count() == 0) {
-            log.info("luodaan kategoria");
-            categoryRepository.save(new bookstore.domain.Category("Fiction"));
-            categoryRepository.save(new bookstore.domain.Category("Non-fiction"));
-            categoryRepository.save(new bookstore.domain.Category("Science"));
-        }
-       
-        log.info("muutama kirja");
-        bookRepository.save(new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925, "9780743273565", 10.99));
-        bookRepository.save(new Book("To Kill a Mockingbird", "Harper Lee", 1960, "9780061120084", 7.99));
-        bookRepository.save(new Book("1984", "George Orwell", 1949, "9780451524935", 8.99));
+            if (categoryRepository.count() == 0) {
+                log.info("luodaan kategoria");
+                categoryRepository.save(new bookstore.domain.Category("Fiction"));
+                categoryRepository.save(new bookstore.domain.Category("Non-fiction"));
+                categoryRepository.save(new bookstore.domain.Category("Science"));
+            }
 
-        log.info("haetaan kaikki kirjat");
-        for (Book book : bookRepository.findAll()) {
-            log.info(book.toString());
-        }
+            if (bookRepository.count() == 0) {
+                log.info("muutama kirja");
+                bookRepository.save(new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925, "9780743273565", 10.99));
+                bookRepository.save(new Book("To Kill a Mockingbird", "Harper Lee", 1960, "9780061120084", 7.99));
+                bookRepository.save(new Book("1984", "George Orwell", 1949, "9780451524935", 8.99));
+            }
 
-        if (userRepository.count()== 0) {
-            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-            userRepository.save(new User("user", encoder.encode("user123"), "user@example.com", "ROLE_USER"));
-            userRepository.save(new User("admin", encoder.encode("admin123"), "admin@example.com", "ROLE_ADMIN"));
-            log.info("Käyttäjät luotu tietokantaan");
+
+            log.info("haetaan kaikki kirjat");
+            for (Book book : bookRepository.findAll()) {
+                log.info(book.toString());
+            }
+
+            if (userRepository.count()== 0) {
+                BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+                userRepository.save(new User("user", encoder.encode(userPassword), "user@example.com", "ROLE_USER"));
+                userRepository.save(new User("admin", encoder.encode(adminPassword), "admin@example.com", "ROLE_ADMIN"));
+                log.info("Käyttäjät luotu tietokantaan");
         }
     };
 
--- src/main/java/bookstore/SecurityConfig.java
+++ src/main/java/bookstore/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.Customizer;
 
 @Configuration
 @EnableWebSecurity
@@ -19,8 +20,11 @@ public class SecurityConfig {
         http
             .authorizeHttpRequests((authorize) -> authorize
             .requestMatchers("/login").permitAll()
+            
+            .requestMatchers("/h2-console/**").permitAll()
             .requestMatchers("/books/delete/**").hasRole("ADMIN")
             .anyRequest().authenticated())
+            .httpBasic(Customizer.withDefaults())
 
             .formLogin((form) -> form
             .loginPage("/login")
@@ -30,6 +34,7 @@ public class SecurityConfig {
             .logout((logout) -> logout
             .permitAll());
 
+            
         return http.build();
 
     }
